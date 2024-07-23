4407f2a6da926f3245638c24b504c8cd9448d90a
WhyMe <WhyMe@i.ua>
2024-07-23 20:38:49 +0200

    Add Navbar and Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home/Home'
 
 function App() {
-
-
-  return (
-    <>
-      <h1>Hallo</h1>
-    </>
-  )
+	return (
+		<>
+			<header className='main-header'>
+				<div className='navbar-container'>
+					<Navbar />
+				</div>
+			</header>
+			<div className='app'>
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+			</div>
+		</>
+	)
 }
 
 export default App
