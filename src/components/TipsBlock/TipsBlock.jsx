ef9a0fd512878eb096787d863a20909b993e8a7e
WhyMe <WhyMe@i.ua>
2024-07-24 20:20:37 +0200

    add TipsBlock

--- src/components/TipsBlock/TipsBlock.jsx
+++ src/components/TipsBlock/TipsBlock.jsx
@@ -1,55 +1,51 @@
-import React from 'react'
+
 import './TipsBlock.css'
 import ButtonViewAll from '../ButtonViewAll/ButtonViewAll'
 import { assets } from '../../assets/assets'
 
 const TipsBlock = () => {
 	return (
-		// <div className='tips-main'>
-		// 	<div className='tips-title'>
-		// 		<h2>Our tips and advice</h2>
-		// 		<ButtonViewAll />
-		// 	</div>
-		// 	<div className='tips-block'>
-		// 		<img src={assets.tips1} alt='' />
-		// 		<div className='tips-content'>
-		// 			<div className='tips-content-title'>
-		// 				<h3>Enjoy the atmosphere and unique flavors of our café.</h3>
-		// 			</div>
-		// 			<div className='tips-content-reviews'>
-		// 				<p>
-		// 					We take pride in our cozy setting and the variety of dishes on our
-		// 					menu. Be sure to try our daily specials to discover new culinary
-		// 					experiences.
-		// 				</p>
-		// 			</div>
-		// 		</div>
-		// 	</div>
-
-		// </div>
-
 		<section className='advice-tips'>
-			<div className='container-tips'>
-				<div className='flex-tips'>
-					<div className='title-tips-32'>Our tips and advice</div>
-					<ButtonViewAll />
-				</div>
+			<div className='flex-tips'>
+				<div className='title-tips-32'>Our tips and advice</div>
+				<ButtonViewAll />
+			</div>
 
+			<div className='container-tips'>
 				<div className='advice-blocks-tips'>
-					<img src={assets.tips1} alt='' />
-					<div className='advice-block-tips'>
-						<div className='title-tips-24'>
-							Enjoy the atmosphere and unique flavors of our café.
-						</div>
-						<div className='title-tips-16'>
-							We take pride in our cozy setting and the variety of dishes on our
-							menu. Be sure to try our daily specials to discover new
-							culinaryexperiences.
+					<div className='advice-blocks-content'>
+						<img src={assets.tips1} alt='' />
+						<div className='advice-block-tips'>
+							<div className='title-tips-24'>
+								Enjoy the atmosphere and unique flavors of our café.
+							</div>
+							<div className='title-tips-16'>
+								We take pride in our cozy setting and the variety of dishes on
+								our menu. Be sure to try our daily specials to discover new
+								culinaryexperiences.
+							</div>
 						</div>
 					</div>
 				</div>
 
-                
+				<div className='advice-blocks-tips'>
+					<div className='advice-blocks-content'>
+						<img src={assets.tips2} alt='' />
+						<div className='advice-block-tips'>
+							<div className='title-tips-24'>
+								Take some time for yourself and your health.
+							</div>
+							<div className='title-tips-16'>
+								Try our fresh and healthy dishes, made from local ingredients.
+								Our café always offers something delicious and nutritious for
+								your body. Whether you're looking for a light snack or a full
+								meal, you'll find options that cater to your dietary preferences
+								and needs. Enjoy a moment of relaxation with our refreshing
+								beverages, perfect for any time of the day.
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 	)
